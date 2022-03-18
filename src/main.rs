595e99c67fa4f86ee9608a9d61fa23d313fdcd7e
René Ladan <rene0+github@freedom.nl>
2022-03-19 00:47:59 +0100

    Remove unused SparkFun/no-BSP stuff.

--- src/main.rs
+++ src/main.rs
@@ -9,11 +9,7 @@ use defmt_rtt as _;
 use embedded_hal::digital::v2::OutputPin;
 use embedded_time::fixed_point::FixedPoint;
 use panic_halt as _;
-
-// Provide an alias for our BSP so we can switch targets quickly.
-// Uncomment the BSP you included in Cargo.toml, the rest of the code does not need to change.
 use rp_pico as bsp;
-// use sparkfun_pro_micro_rp2040 as bsp;
 
 use bsp::hal::{
     clocks::{init_clocks_and_plls, Clock},

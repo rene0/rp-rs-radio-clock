6e202c134ed3a5aca7d614be407bb8503914aeec
René Ladan <rene0+github@freedom.nl>
2022-04-28 14:52:10 +0200

    test-ky040: preliminary test for the direction of the KY-040.

--- hardware-tests/test-ky040/src/main.rs
+++ hardware-tests/test-ky040/src/main.rs
@@ -10,7 +10,13 @@ use rp_pico as bsp;
 
 fn test_ky040(pins: bsp::Pins) -> ! {
     let button_pin = pins.gpio7.into_pull_down_input(); // KY-040 SW push button, perhaps into_floating_input() works too
+    let dt_pin = pins.gpio8.into_pull_down_input();
+    let clk_pin = pins.gpio9.into_pull_down_input();
     let mut led_pin = pins.led.into_push_pull_output();
+    let mut npl_led_bit_a = pins.gpio3.into_push_pull_output();
+    let mut npl_led_bit_b = pins.gpio4.into_push_pull_output();
+    let mut rotation_start = clk_pin.is_low().unwrap();
+    let mut rotation_now;
     // test "write to I/O-port" and pushing a button --> OK
     loop {
         if button_pin.is_low().unwrap() {
@@ -18,6 +24,21 @@ fn test_ky040(pins: bsp::Pins) -> ! {
         } else {
             led_pin.set_low().unwrap();
         }
+        rotation_now = clk_pin.is_low().unwrap();
+        if rotation_now != rotation_start {
+            // Use the DT pin to found out the direction.
+            // This probably needs debouncing.
+            if dt_pin.is_low().unwrap() != rotation_now {
+                // clockwise
+                npl_led_bit_a.set_low().unwrap();
+                npl_led_bit_b.set_high().unwrap();
+            } else {
+                // counterclockwise
+                npl_led_bit_a.set_high().unwrap();
+                npl_led_bit_b.set_low().unwrap();
+            }
+        }
+        rotation_start = rotation_now;
     }
 }
 

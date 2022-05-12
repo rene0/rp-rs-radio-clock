2aca1dd6612a8b9da14bf0ab66327f7fb882ef92
René Ladan <rene0+github@freedom.nl>
2022-05-12 17:13:43 +0200

    add test for simulated periodic timer.
    It toggles the GPIO LED twice per second, despite the
    250,000 microseconds given (toggling algorithm?)

---
+++ hardware-tests/test-isr-timer/src/main.rs
@@ -0,0 +1,76 @@
+#![no_std]
+#![no_main]
+
+use bsp::hal::{pac, sio::Sio};
+use bsp::pac::interrupt;
+use core::cell::RefCell;
+use core::sync::atomic::{AtomicBool, Ordering};
+use cortex_m::interrupt::Mutex;
+use cortex_m_rt::entry; // the macro for our start-up function
+use defmt_rtt as _; // otherwise "linking with `flip-link`" fails
+use embedded_hal::digital::v2::OutputPin;
+use embedded_time::duration::Extensions;
+use panic_halt as _;
+use rp_pico as bsp;
+
+use rp_pico::hal::timer::Alarm0;
+
+static GLOBAL_PINS: Mutex<RefCell<Option<Alarm0>>> = Mutex::new(RefCell::new(None));
+static G_TOGGLE_LED: AtomicBool = AtomicBool::new(false);
+/// Entry point to our bare-metal application.
+///
+/// The `#[entry]` macro ensures the Cortex-M start-up code calls this function
+/// as soon as all global variables are initialised.
+#[entry]
+fn main() -> ! {
+    let mut pac = pac::Peripherals::take().unwrap();
+    let sio = Sio::new(pac.SIO);
+    // boilerplate from the rp2040 template:
+    let pins = bsp::Pins::new(
+        pac.IO_BANK0,
+        pac.PADS_BANK0,
+        sio.gpio_bank0,
+        &mut pac.RESETS,
+    );
+    let mut led_pin = pins.led.into_push_pull_output();
+    led_pin.set_low().unwrap();
+    let mut timer = bsp::hal::Timer::new(pac.TIMER, &mut pac.RESETS);
+    let mut alarm0 = timer.alarm_0().unwrap();
+    alarm0.enable_interrupt();
+    alarm0.schedule(250_000.microseconds()).unwrap();
+    cortex_m::interrupt::free(|cs| GLOBAL_PINS.borrow(cs).replace(Some(alarm0)));
+    #[allow(unsafe_code)]
+    unsafe {
+        pac::NVIC::unmask(pac::Interrupt::TIMER_IRQ_0);
+    }
+    let mut led_active = false;
+    loop {
+        if G_TOGGLE_LED.load(Ordering::Acquire) {
+            G_TOGGLE_LED.store(false, Ordering::Release);
+            if led_active {
+                led_pin.set_high().unwrap();
+            } else {
+                led_pin.set_low().unwrap();
+            }
+            led_active = !led_active;
+        }
+    }
+}
+
+#[interrupt]
+fn TIMER_IRQ_0() {
+    static mut ALARM: Option<Alarm0> = None;
+
+    if ALARM.is_none() {
+        // one-time lazy init
+        cortex_m::interrupt::free(|cs| {
+            *ALARM = GLOBAL_PINS.borrow(cs).take();
+        });
+    }
+    if let Some(alarm) = ALARM {
+        G_TOGGLE_LED.store(true, Ordering::Release);
+        alarm.clear_interrupt();
+        // alarm is oneshot, so re-arm it here:
+        alarm.schedule(250_000.microseconds()).unwrap();
+    }
+}

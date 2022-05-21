1d7ee96977c4f12cbbed9366fcada87832c708b6
René Ladan <rene0+github@freedom.nl>
2022-05-22 00:32:50 +0200

    Add edge-and-timer mode.
    - show the time (in microseconds since boot) of the incoming low and high edges of both DCF77 and NPL.
    - let the on-board LED blink at 250ms intervals.
    
    This mode could be a "debug" mode reachable via the KY-040.
    
    The ISRs and the code to unmask the interrupts should be the only unsafe code in this project.

--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,30 @@
 #![no_std]
 #![no_main]
 
+use bsp::hal::gpio;
+use bsp::hal::gpio::Interrupt::{EdgeHigh, EdgeLow};
+use bsp::hal::timer::Alarm0;
 use bsp::hal::{
     clocks::{init_clocks_and_plls, Clock},
     pac,
     sio::Sio,
     watchdog::Watchdog,
 };
+use bsp::pac::interrupt;
 use bsp::XOSC_CRYSTAL_FREQ;
+use core::cell::RefCell;
+use core::fmt::Write;
+use core::sync::atomic::{AtomicBool, AtomicU32, Ordering};
+use cortex_m::interrupt::Mutex;
 use cortex_m_rt::entry; // the macro for our start-up function
 use defmt_rtt as _; // otherwise "linking with `flip-link`" fails
-use embedded_hal::digital::v2::OutputPin;
+use embedded_hal::digital::v2::{InputPin, OutputPin};
+use embedded_time::duration::Extensions;
 use embedded_time::fixed_point::FixedPoint; // for .integer()
 use embedded_time::rate::Extensions as rate_extensions; // allows for plain "400" in "400.kHz()"
 use hd44780_driver::{Cursor, CursorBlink, HD44780};
 use hd44780_helpers::Hd44780Wrapper;
+use heapless::String;
 use panic_halt as _;
 use rp_pico as bsp;
 
@@ -25,6 +35,26 @@ const I2C_ADDRESS: u8 = 0x27;
 const DISPLAY_ROWS: u8 = 4;
 /// Number of columns on the display, change as needed
 const DISPLAY_COLUMNS: u8 = 20;
+static G_HIGH_EDGE_RECEIVED_DCF77: AtomicBool = AtomicBool::new(false);
+static G_LOW_EDGE_RECEIVED_DCF77: AtomicBool = AtomicBool::new(false);
+static G_HIGH_EDGE_RECEIVED_NPL: AtomicBool = AtomicBool::new(false);
+static G_LOW_EDGE_RECEIVED_NPL: AtomicBool = AtomicBool::new(false);
+// There is no AtomicU64, so use two halves and do some manual work:
+static G_TIMER_TICK_HIGH_DCF77: AtomicU32 = AtomicU32::new(0);
+static G_TIMER_TICK_LOW_DCF77: AtomicU32 = AtomicU32::new(0);
+static G_TIMER_TICK_HIGH_NPL: AtomicU32 = AtomicU32::new(0);
+static G_TIMER_TICK_LOW_NPL: AtomicU32 = AtomicU32::new(0);
+// tick-tock
+static G_TOGGLE_LED: AtomicBool = AtomicBool::new(false);
+
+type DCF77SignalPin = gpio::Pin<gpio::bank0::Gpio11, gpio::PullDownInput>;
+type NPLSignalPin = gpio::Pin<gpio::bank0::Gpio6, gpio::PullDownInput>;
+// needed to transfer our pin(s) into the ISR:
+static GLOBAL_PIN_DCF77: Mutex<RefCell<Option<DCF77SignalPin>>> = Mutex::new(RefCell::new(None));
+static GLOBAL_PIN_NPL: Mutex<RefCell<Option<NPLSignalPin>>> = Mutex::new(RefCell::new(None));
+static GLOBAL_TIMER: Mutex<RefCell<Option<bsp::hal::Timer>>> = Mutex::new(RefCell::new(None));
+// and one for the timer alarm:
+static GLOBAL_ALARM: Mutex<RefCell<Option<Alarm0>>> = Mutex::new(RefCell::new(None));
 
 /// Entry point to our bare-metal application.
 ///
@@ -101,6 +131,175 @@ fn main() -> ! {
     npl_led_bit_b.set_low().unwrap();
     npl_led_error.set_high().unwrap();
 
-    #[allow(clippy::empty_loop)]
-    loop {}
+    let mut led_pin = pins.led.into_push_pull_output();
+    led_pin.set_low().unwrap();
+
+    let dcf77_signal_pin = pins.gpio11.into_mode();
+    dcf77_signal_pin.set_interrupt_enabled(EdgeHigh, true);
+    dcf77_signal_pin.set_interrupt_enabled(EdgeLow, true);
+    let npl_signal_pin = pins.gpio6.into_mode();
+    npl_signal_pin.set_interrupt_enabled(EdgeHigh, true);
+    npl_signal_pin.set_interrupt_enabled(EdgeLow, true);
+    let mut timer = bsp::hal::Timer::new(pac.TIMER, &mut pac.RESETS);
+    let mut alarm0 = timer.alarm_0().unwrap();
+
+    // Give our pin and timer away to the ISR
+    cortex_m::interrupt::free(|cs| GLOBAL_PIN_DCF77.borrow(cs).replace(Some(dcf77_signal_pin)));
+    cortex_m::interrupt::free(|cs| GLOBAL_PIN_NPL.borrow(cs).replace(Some(npl_signal_pin)));
+    cortex_m::interrupt::free(|cs| GLOBAL_TIMER.borrow(cs).replace(Some(timer)));
+    alarm0.enable_interrupt();
+    alarm0.schedule(250_000.microseconds()).unwrap();
+    cortex_m::interrupt::free(|cs| GLOBAL_ALARM.borrow(cs).replace(Some(alarm0)));
+    // Ready, set, go!
+    unsafe {
+        pac::NVIC::unmask(pac::Interrupt::IO_IRQ_BANK0);
+        pac::NVIC::unmask(pac::Interrupt::TIMER_IRQ_0);
+    }
+    let mut time_high: u64;
+    let mut time_low: u64;
+    let mut led_active = false;
+    loop {
+        if G_LOW_EDGE_RECEIVED_DCF77.load(Ordering::Acquire) {
+            time_low = (G_TIMER_TICK_HIGH_DCF77.load(Ordering::Acquire) as u64) << 32
+                | (G_TIMER_TICK_LOW_DCF77.load(Ordering::Acquire) as u64);
+            lcd.set_cursor_pos(lcd_helper.get_xy(10, 0).unwrap(), &mut delay)
+                .unwrap();
+            lcd.write_char('L', &mut delay).unwrap();
+            lcd.set_cursor_pos(lcd_helper.get_xy(0, 1).unwrap(), &mut delay)
+                .unwrap();
+            let mut data = String::<20>::from("");
+            let _ = write!(data, "{}", time_low);
+            lcd.write_str(data.as_str(), &mut delay).unwrap();
+            G_LOW_EDGE_RECEIVED_DCF77.store(false, Ordering::Release);
+        }
+        if G_LOW_EDGE_RECEIVED_NPL.load(Ordering::Acquire) {
+            time_low = (G_TIMER_TICK_HIGH_NPL.load(Ordering::Acquire) as u64) << 32
+                | (G_TIMER_TICK_LOW_NPL.load(Ordering::Acquire) as u64);
+            lcd.set_cursor_pos(lcd_helper.get_xy(10, 2).unwrap(), &mut delay)
+                .unwrap();
+            lcd.write_char('L', &mut delay).unwrap();
+            lcd.set_cursor_pos(lcd_helper.get_xy(0, 3).unwrap(), &mut delay)
+                .unwrap();
+            let mut data = String::<20>::from("");
+            let _ = write!(data, "{}", time_low);
+            lcd.write_str(data.as_str(), &mut delay).unwrap();
+            G_LOW_EDGE_RECEIVED_NPL.store(false, Ordering::Release);
+        }
+        if G_HIGH_EDGE_RECEIVED_DCF77.load(Ordering::Acquire) {
+            time_high = (G_TIMER_TICK_HIGH_DCF77.load(Ordering::Acquire) as u64) << 32
+                | (G_TIMER_TICK_LOW_DCF77.load(Ordering::Acquire) as u64);
+            lcd.set_cursor_pos(lcd_helper.get_xy(10, 0).unwrap(), &mut delay)
+                .unwrap();
+            lcd.write_char('H', &mut delay).unwrap();
+            lcd.set_cursor_pos(lcd_helper.get_xy(0, 1).unwrap(), &mut delay)
+                .unwrap();
+            let mut data = String::<20>::from("");
+            let _ = write!(data, "{}", time_high);
+            lcd.write_str(data.as_str(), &mut delay).unwrap();
+            G_HIGH_EDGE_RECEIVED_DCF77.store(false, Ordering::Release);
+        }
+        if G_HIGH_EDGE_RECEIVED_NPL.load(Ordering::Acquire) {
+            time_high = (G_TIMER_TICK_HIGH_NPL.load(Ordering::Acquire) as u64) << 32
+                | (G_TIMER_TICK_LOW_NPL.load(Ordering::Acquire) as u64);
+            lcd.set_cursor_pos(lcd_helper.get_xy(10, 2).unwrap(), &mut delay)
+                .unwrap();
+            lcd.write_char('H', &mut delay).unwrap();
+            lcd.set_cursor_pos(lcd_helper.get_xy(0, 3).unwrap(), &mut delay)
+                .unwrap();
+            let mut data = String::<20>::from("");
+            let _ = write!(data, "{}", time_high);
+            lcd.write_str(data.as_str(), &mut delay).unwrap();
+            G_HIGH_EDGE_RECEIVED_NPL.store(false, Ordering::Release);
+        }
+        if G_TOGGLE_LED.load(Ordering::Acquire) {
+            if led_active {
+                led_pin.set_high().unwrap();
+            } else {
+                led_pin.set_low().unwrap();
+            }
+            led_active = !led_active;
+            G_TOGGLE_LED.store(false, Ordering::Release);
+        }
+    }
+}
+
+#[interrupt]
+fn IO_IRQ_BANK0() {
+    static mut TICK_TIMER: Option<bsp::hal::Timer> = None;
+    static mut DCF77_PIN: Option<DCF77SignalPin> = None;
+    static mut PREVIOUS_DCF77_LOW: bool = false;
+    static mut NPL_PIN: Option<NPLSignalPin> = None;
+    static mut PREVIOUS_NPL_LOW: bool = false;
+
+    // This is one-time lazy initialisation. We steal the variables given to us via `GLOBAL_*`.
+    if TICK_TIMER.is_none() {
+        cortex_m::interrupt::free(|cs| *TICK_TIMER = GLOBAL_TIMER.borrow(cs).take());
+    }
+    if DCF77_PIN.is_none() {
+        cortex_m::interrupt::free(|cs| *DCF77_PIN = GLOBAL_PIN_DCF77.borrow(cs).take());
+    }
+    if NPL_PIN.is_none() {
+        cortex_m::interrupt::free(|cs| *NPL_PIN = GLOBAL_PIN_NPL.borrow(cs).take());
+    }
+
+    // Our edge interrupts don't clear themselves.
+    // Do that at the end of the various conditions so we don't immediately jump back to this interrupt handler.
+    if let Some(tick) = TICK_TIMER {
+        let now = tick.get_counter();
+        if let Some(dcf77_signal) = DCF77_PIN {
+            if dcf77_signal.is_low().unwrap() {
+                if !*PREVIOUS_DCF77_LOW {
+                    *PREVIOUS_DCF77_LOW = true;
+                    G_LOW_EDGE_RECEIVED_DCF77.store(true, Ordering::Release);
+                    G_TIMER_TICK_HIGH_DCF77.store((now >> 32) as u32, Ordering::Release);
+                    G_TIMER_TICK_LOW_DCF77.store(now as u32, Ordering::Release);
+                }
+                dcf77_signal.clear_interrupt(EdgeLow);
+            } else {
+                if *PREVIOUS_DCF77_LOW {
+                    *PREVIOUS_DCF77_LOW = false;
+                    G_HIGH_EDGE_RECEIVED_DCF77.store(true, Ordering::Release);
+                    G_TIMER_TICK_HIGH_DCF77.store((now >> 32) as u32, Ordering::Release);
+                    G_TIMER_TICK_LOW_DCF77.store(now as u32, Ordering::Release);
+                }
+                dcf77_signal.clear_interrupt(EdgeHigh);
+            }
+        }
+
+        if let Some(npl_signal) = NPL_PIN {
+            if npl_signal.is_low().unwrap() {
+                if !*PREVIOUS_NPL_LOW {
+                    *PREVIOUS_NPL_LOW = true;
+                    G_TIMER_TICK_HIGH_NPL.store((now >> 32) as u32, Ordering::Release);
+                    G_TIMER_TICK_LOW_NPL.store(now as u32, Ordering::Release);
+                    G_LOW_EDGE_RECEIVED_NPL.store(true, Ordering::Release);
+                }
+                npl_signal.clear_interrupt(EdgeLow);
+            } else {
+                if *PREVIOUS_NPL_LOW {
+                    *PREVIOUS_NPL_LOW = false;
+                    G_TIMER_TICK_HIGH_NPL.store((now >> 32) as u32, Ordering::Release);
+                    G_TIMER_TICK_LOW_NPL.store(now as u32, Ordering::Release);
+                    G_HIGH_EDGE_RECEIVED_NPL.store(true, Ordering::Release);
+                }
+                npl_signal.clear_interrupt(EdgeHigh);
+            }
+        }
+    }
+}
+
+#[interrupt]
+fn TIMER_IRQ_0() {
+    static mut ALARM: Option<Alarm0> = None;
+
+    if ALARM.is_none() {
+        // one-time lazy initialization
+        cortex_m::interrupt::free(|cs| *ALARM = GLOBAL_ALARM.borrow(cs).take());
+    }
+    if let Some(alarm) = ALARM {
+        G_TOGGLE_LED.store(true, Ordering::Release);
+        alarm.clear_interrupt();
+        // alarm is oneshot, so re-arm it here:
+        alarm.schedule(250_000.microseconds()).unwrap();
+    }
 }

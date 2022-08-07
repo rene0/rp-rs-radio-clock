fbb63a842cad6a94691289195d08df546b6a36bc
René Ladan <rene0+github@freedom.nl>
2022-08-07 21:38:37 +0200

    Split off some front-end related code to a frontend/dcf77.rs
    This looks a bit superfluous now but with NPL code added it should save a lot of clutter.
    It also makes it easier to add another receiver
    frontend (e.g. TDF).

---
+++ src/frontend/dcf77.rs
@@ -0,0 +1,194 @@
+use dcf77_utils::DCF77Utils;
+
+use embedded_hal::digital::v2::OutputPin;
+use rp_pico::hal::gpio;
+use core::cmp::Ordering as spaceship;
+
+pub fn update_leds(
+    dcf77: &DCF77Utils,
+    led_time: &mut gpio::Pin<gpio::pin::bank0::Gpio12, gpio::PushPullOutput>,
+    led_bit: &mut gpio::Pin<gpio::pin::bank0::Gpio13, gpio::PushPullOutput>,
+    led_error: &mut gpio::Pin<gpio::pin::bank0::Gpio14, gpio::PushPullOutput>,
+) {
+    if dcf77.get_ind_time() {
+        led_time.set_high().unwrap();
+    } else {
+        led_time.set_low().unwrap();
+    }
+    if dcf77.get_ind_bit() {
+        led_bit.set_high().unwrap();
+    } else {
+        led_bit.set_low().unwrap();
+    }
+    if dcf77.get_ind_error() {
+        led_error.set_high().unwrap();
+    } else {
+        led_error.set_low().unwrap();
+    }
+}
+
+
+/// Return if the year has jumped unexpectedly, 'y' or ' '
+pub fn str_jump_year(dcf77: &DCF77Utils) -> char {
+    if dcf77.get_radio_datetime().get_jump_year() {
+        'y'
+    } else {
+        ' '
+    }
+}
+
+/// Return if the month has jumped unexpectedly, 'm' or ' '
+pub fn str_jump_month(dcf77: &DCF77Utils) -> char {
+    if dcf77.get_radio_datetime().get_jump_month() {
+        'm'
+    } else {
+        ' '
+    }
+}
+
+/// Return if the day-of-month has jumped unexpectedly, 'd' or ' '.
+pub fn str_jump_day(dcf77: &DCF77Utils) -> char {
+    if dcf77.get_radio_datetime().get_jump_day() {
+        'd'
+    } else {
+        ' '
+    }
+}
+
+/// Return if the day-of-week has jumped unexpectedly, 'w' or ' '.
+pub fn str_jump_weekday(dcf77: &DCF77Utils) -> char {
+    if dcf77.get_radio_datetime().get_jump_weekday() {
+        'w'
+    } else {
+        ' '
+    }
+}
+
+/// Return if the hour has jumped unexpectedly, 'h' or ' '.
+pub fn str_jump_hour(dcf77: &DCF77Utils) -> char {
+    if dcf77.get_radio_datetime().get_jump_hour() {
+        'h'
+    } else {
+        ' '
+    }
+}
+
+/// Return if the minute has jumped unexpectedly, 'm' or ' '.
+pub fn str_jump_minute(dcf77: &DCF77Utils) -> char {
+    if dcf77.get_radio_datetime().get_jump_minute() {
+        'm'
+    } else {
+        ' '
+    }
+}
+
+/// Return if the daylight saving time status jumped unexpectedly, 't' or ' '.
+pub fn str_jump_dst(dcf77: &DCF77Utils) -> char {
+    if dcf77.get_radio_datetime().get_dst().is_some()
+        && (dcf77.get_radio_datetime().get_dst().unwrap() & radio_datetime_utils::DST_JUMP) != 0
+    {
+        't'
+    } else {
+        ' '
+    }
+}
+
+/// Returns a character representation of the current DST status.
+pub fn str_dst(dcf77: &DCF77Utils) -> char {
+    if let Some(dst) = dcf77.get_radio_datetime().get_dst() {
+        let mut res_dst = if (dst & radio_datetime_utils::DST_SUMMER) != 0 {
+            's'
+        } else {
+            'w'
+        };
+        if (dst & (radio_datetime_utils::DST_ANNOUNCED | radio_datetime_utils::DST_PROCESSED)) != 0
+        {
+            res_dst = res_dst.to_ascii_uppercase();
+        }
+        res_dst
+    } else {
+        '*'
+    }
+}
+
+/// Return a character representation of the minute length status.
+pub fn str_minute_length(dcf77: &DCF77Utils) -> char {
+    match dcf77.get_second().cmp(&dcf77.get_minute_length()) {
+        spaceship::Less => '<',
+        spaceship::Equal => ' ',
+        spaceship::Greater => '>',
+    }
+}
+
+/// Get a textual version of the minute parity bit, ' ' for OK or '1' for error.
+pub fn str_parity_1(dcf77: &DCF77Utils) -> char {
+    if dcf77.get_parity_1() == Some(false) {
+        ' '
+    } else {
+        '1'
+    }
+}
+
+/// Get a textual version of the hour parity bit, ' ' for OK or '2' for error.
+pub fn str_parity_2(dcf77: &DCF77Utils) -> char {
+    if dcf77.get_parity_2() == Some(false) {
+        ' '
+    } else {
+        '2'
+    }
+}
+
+/// Get a textual version of the date parity bit, ' ' for OK or '3' for error.
+pub fn str_parity_3(dcf77: &DCF77Utils) -> char {
+    if dcf77.get_parity_3() == Some(false) {
+        ' '
+    } else {
+        '3'
+    }
+}
+
+/// Return a character representation of the bit 0 status
+pub fn str_bit_0(dcf77: &DCF77Utils) -> char {
+    if dcf77.get_bit_0() == Some(false) {
+        ' '
+    } else {
+        'M'
+    }
+}
+
+/// Return a character representation of the call bit status
+pub fn str_call_bit(dcf77: &DCF77Utils) -> char {
+    if dcf77.get_call_bit() == Some(true) {
+        'C'
+    } else {
+        ' '
+    }
+}
+
+/// Return a character representation of the bit 20 status
+pub fn str_bit_20(dcf77: &DCF77Utils) -> char {
+    if dcf77.get_bit_20() == Some(true) {
+        ' '
+    } else {
+        'S'
+    }
+}
+
+/// Returns a character representation of the current leap second status.
+pub fn str_leap_second(dcf77: &DCF77Utils) -> char {
+    if let Some(leap_second) = dcf77.get_radio_datetime().get_leap_second() {
+        if (leap_second & radio_datetime_utils::LEAP_PROCESSED) != 0 {
+            'L'
+        } else if (leap_second & radio_datetime_utils::LEAP_NON_ZERO) != 0 {
+            '1'
+        } else if (leap_second & radio_datetime_utils::LEAP_MISSING) != 0 {
+            'm'
+        } else if (leap_second & radio_datetime_utils::LEAP_ANNOUNCED) != 0 {
+            'l'
+        } else {
+            ' ' // LEAP_NONE
+        }
+    } else {
+        '*'
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,6 @@ use bsp::hal::{
 use bsp::{entry, XOSC_CRYSTAL_FREQ};
 use core::{
     cell::RefCell,
-    cmp::Ordering as spaceship,
     fmt::Write,
     sync::atomic::{AtomicBool, AtomicU32, Ordering},
 };
@@ -39,6 +38,9 @@ use hd44780_driver::{
 use heapless::String;
 use panic_halt as _;
 use rp_pico as bsp;
+use crate::frontend::*;
+
+mod frontend;
 
 /// I²C address of the PCF8574 adapter, change as needed
 const I2C_ADDRESS: u8 = 0x27;
@@ -142,7 +144,7 @@ fn main() -> ! {
     let mut dcf77_led_bit = pins.gpio13.into_push_pull_output();
     let mut dcf77_led_error = pins.gpio14.into_push_pull_output();
     let mut dcf77 = DCF77Utils::new(FRAMES_PER_SECOND);
-    update_leds_dcf77(
+    dcf77::update_leds(
         &dcf77,
         &mut dcf77_led_time,
         &mut dcf77_led_bit,
@@ -195,7 +197,7 @@ fn main() -> ! {
                 show_pulses(&mut lcd, &mut delay, 0, is_low_edge, t0_dcf77, t1_dcf77);
             }
             dcf77.handle_new_edge(is_low_edge, t0_dcf77, t1_dcf77);
-            update_leds_dcf77(
+            dcf77::update_leds(
                 &dcf77,
                 &mut dcf77_led_time,
                 &mut dcf77_led_bit,
@@ -220,7 +222,7 @@ fn main() -> ! {
         if G_TIMER_TICK.load(Ordering::Acquire) {
             led_pin.toggle().unwrap();
             dcf77.handle_new_timer_tick();
-            update_leds_dcf77(
+            dcf77::update_leds(
                 &dcf77,
                 &mut dcf77_led_time,
                 &mut dcf77_led_bit,
@@ -233,19 +235,19 @@ fn main() -> ! {
                     let _ = write!(
                         str_buf,
                         "{}{}{}{}{}{}{}{}{}{} {}{}{}",
-                        str_jump_year(&dcf77),
-                        str_jump_month(&dcf77),
-                        str_jump_day(&dcf77),
-                        str_jump_weekday(&dcf77),
-                        str_jump_hour(&dcf77),
-                        str_jump_minute(&dcf77),
-                        str_jump_dst(&dcf77),
-                        str_parity_3(&dcf77),
-                        str_parity_2(&dcf77),
-                        str_parity_1(&dcf77),
-                        str_bit_0(&dcf77),
-                        str_bit_20(&dcf77),
-                        str_minute_length(&dcf77),
+                        dcf77::str_jump_year(&dcf77),
+                        dcf77::str_jump_month(&dcf77),
+                        dcf77::str_jump_day(&dcf77),
+                        dcf77::str_jump_weekday(&dcf77),
+                        dcf77::str_jump_hour(&dcf77),
+                        dcf77::str_jump_minute(&dcf77),
+                        dcf77::str_jump_dst(&dcf77),
+                        dcf77::str_parity_3(&dcf77),
+                        dcf77::str_parity_2(&dcf77),
+                        dcf77::str_parity_1(&dcf77),
+                        dcf77::str_bit_0(&dcf77),
+                        dcf77::str_bit_20(&dcf77),
+                        dcf77::str_minute_length(&dcf77),
                     );
                     lcd.set_cursor_pos(get_xy(6, 0).unwrap(), &mut delay)
                         .unwrap();
@@ -270,9 +272,9 @@ fn main() -> ! {
                     let _ = write!(
                         str_buf,
                         "{}{}{}",
-                        str_call_bit(&dcf77),
-                        str_dst(&dcf77),
-                        str_leap_second(&dcf77)
+                        dcf77::str_call_bit(&dcf77),
+                        dcf77::str_dst(&dcf77),
+                        dcf77::str_leap_second(&dcf77)
                     );
                     lcd.set_cursor_pos(get_xy(17, 1).unwrap(), &mut delay)
                         .unwrap();
@@ -314,171 +316,6 @@ fn str_weekday(weekday: Option<u8>) -> String<2> {
     })
 }
 
-/// Return if the year has jumped unexpectedly, 'y' or ' '
-fn str_jump_year(dcf77: &DCF77Utils) -> char {
-    if dcf77.get_radio_datetime().get_jump_year() {
-        'y'
-    } else {
-        ' '
-    }
-}
-
-/// Return if the month has jumped unexpectedly, 'm' or ' '
-fn str_jump_month(dcf77: &DCF77Utils) -> char {
-    if dcf77.get_radio_datetime().get_jump_month() {
-        'm'
-    } else {
-        ' '
-    }
-}
-
-/// Return if the day-of-month has jumped unexpectedly, 'd' or ' '.
-fn str_jump_day(dcf77: &DCF77Utils) -> char {
-    if dcf77.get_radio_datetime().get_jump_day() {
-        'd'
-    } else {
-        ' '
-    }
-}
-
-/// Return if the day-of-week has jumped unexpectedly, 'w' or ' '.
-fn str_jump_weekday(dcf77: &DCF77Utils) -> char {
-    if dcf77.get_radio_datetime().get_jump_weekday() {
-        'w'
-    } else {
-        ' '
-    }
-}
-
-/// Return if the hour has jumped unexpectedly, 'h' or ' '.
-fn str_jump_hour(dcf77: &DCF77Utils) -> char {
-    if dcf77.get_radio_datetime().get_jump_hour() {
-        'h'
-    } else {
-        ' '
-    }
-}
-
-/// Return if the minute has jumped unexpectedly, 'm' or ' '.
-fn str_jump_minute(dcf77: &DCF77Utils) -> char {
-    if dcf77.get_radio_datetime().get_jump_minute() {
-        'm'
-    } else {
-        ' '
-    }
-}
-
-/// Return if the daylight saving time status jumped unexpectedly, 't' or ' '.
-fn str_jump_dst(dcf77: &DCF77Utils) -> char {
-    if dcf77.get_radio_datetime().get_dst().is_some()
-        && (dcf77.get_radio_datetime().get_dst().unwrap() & radio_datetime_utils::DST_JUMP) != 0
-    {
-        't'
-    } else {
-        ' '
-    }
-}
-
-/// Returns a character representation of the current DST status.
-fn str_dst(dcf77: &DCF77Utils) -> char {
-    if let Some(dst) = dcf77.get_radio_datetime().get_dst() {
-        let mut res_dst = if (dst & radio_datetime_utils::DST_SUMMER) != 0 {
-            's'
-        } else {
-            'w'
-        };
-        if (dst & (radio_datetime_utils::DST_ANNOUNCED | radio_datetime_utils::DST_PROCESSED)) != 0
-        {
-            res_dst = res_dst.to_ascii_uppercase();
-        }
-        res_dst
-    } else {
-        '*'
-    }
-}
-
-/// Returns a character representation of the current leap second status.
-fn str_leap_second(dcf77: &DCF77Utils) -> char {
-    if let Some(leap_second) = dcf77.get_radio_datetime().get_leap_second() {
-        if (leap_second & radio_datetime_utils::LEAP_PROCESSED) != 0 {
-            'L'
-        } else if (leap_second & radio_datetime_utils::LEAP_NON_ZERO) != 0 {
-            '1'
-        } else if (leap_second & radio_datetime_utils::LEAP_MISSING) != 0 {
-            'm'
-        } else if (leap_second & radio_datetime_utils::LEAP_ANNOUNCED) != 0 {
-            'l'
-        } else {
-            ' ' // LEAP_NONE
-        }
-    } else {
-        '*'
-    }
-}
-
-/// Get a textual version of the minute parity bit, ' ' for OK or '1' for error.
-fn str_parity_1(dcf77: &DCF77Utils) -> char {
-    if dcf77.get_parity_1() == Some(false) {
-        ' '
-    } else {
-        '1'
-    }
-}
-
-/// Get a textual version of the hour parity bit, ' ' for OK or '2' for error.
-fn str_parity_2(dcf77: &DCF77Utils) -> char {
-    if dcf77.get_parity_2() == Some(false) {
-        ' '
-    } else {
-        '2'
-    }
-}
-
-/// Get a textual version of the date parity bit, ' ' for OK or '3' for error.
-fn str_parity_3(dcf77: &DCF77Utils) -> char {
-    if dcf77.get_parity_3() == Some(false) {
-        ' '
-    } else {
-        '3'
-    }
-}
-
-/// Return a character representation of the minute length status.
-fn str_minute_length(dcf77: &DCF77Utils) -> char {
-    match dcf77.get_second().cmp(&dcf77.get_minute_length()) {
-        spaceship::Less => '<',
-        spaceship::Equal => ' ',
-        spaceship::Greater => '>',
-    }
-}
-
-/// Return a character representation of the bit 0 status
-fn str_bit_0(dcf77: &DCF77Utils) -> char {
-    if dcf77.get_bit_0() == Some(false) {
-        ' '
-    } else {
-        'M'
-    }
-}
-
-/// Return a character representation of the call bit status
-fn str_call_bit(dcf77: &DCF77Utils) -> char {
-    if dcf77.get_call_bit() == Some(true) {
-        'C'
-    } else {
-        ' '
-    }
-}
-
-/// Return a character representation of the bit 20 status
-fn str_bit_20(dcf77: &DCF77Utils) -> char {
-    if dcf77.get_bit_20() == Some(true) {
-        ' '
-    } else {
-        'S'
-    }
-}
-
 // FIXME skips pulses, updating too slow? HD44780 driver perhaps uses too many delays.
 //       Works fine with just showing the pulse lengths of one station.
 fn show_pulses<D: DelayUs<u16> + DelayMs<u8>>(
@@ -504,29 +341,6 @@ fn show_pulses<D: DelayUs<u16> + DelayMs<u8>>(
     str_buf.clear();
     let _ = write!(str_buf, "{:<10}  ", t1);
     lcd.write_str(str_buf.as_str(), delay).unwrap();
-}
-
-fn update_leds_dcf77(
-    dcf77: &DCF77Utils,
-    led_time: &mut gpio::Pin<gpio::pin::bank0::Gpio12, gpio::PushPullOutput>,
-    led_bit: &mut gpio::Pin<gpio::pin::bank0::Gpio13, gpio::PushPullOutput>,
-    led_error: &mut gpio::Pin<gpio::pin::bank0::Gpio14, gpio::PushPullOutput>,
-) {
-    if dcf77.get_ind_time() {
-        led_time.set_high().unwrap();
-    } else {
-        led_time.set_low().unwrap();
-    }
-    if dcf77.get_ind_bit() {
-        led_bit.set_high().unwrap();
-    } else {
-        led_bit.set_low().unwrap();
-    }
-    if dcf77.get_ind_error() {
-        led_error.set_high().unwrap();
-    } else {
-        led_error.set_low().unwrap();
-    }
 }
 
 /// Gets the one-dimensional HD44780 coordinate for position (x, y) (zero-based)

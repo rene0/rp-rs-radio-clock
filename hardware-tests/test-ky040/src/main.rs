e191900b5cece24b474efec637d2fc52d0ee7c70
René Ladan <rene0+github@freedom.nl>
2022-12-17 17:12:43 +0100

    test-ky040: use positive logic
    The KY040 has builtin pull-up resistors for DT and CLK.
    SW lacks an external resistor so needs the pull-up resistor from the RPi Pico. Note that using into_pull_down_input() always gives LOW output and that the SW logic is negative.

--- hardware-tests/test-ky040/src/main.rs
+++ hardware-tests/test-ky040/src/main.rs
@@ -15,27 +15,28 @@ use rp_pico as bsp;
 // https://www.best-microcontroller-projects.com/rotary-encoder.html
 // https://arduino.stackexchange.com/questions/69307/ky-040-rotary-encoder-skipping-steps
 fn test_ky040(pins: bsp::Pins) -> ! {
-    let sw_pin = pins.gpio7.into_pull_down_input(); // perhaps into_floating_input() works too
-    let dt_pin = pins.gpio8.into_pull_down_input();
-    let clk_pin = pins.gpio9.into_pull_down_input();
+    let sw_pin = pins.gpio7.into_pull_up_input(); // into_pull_down_input() always gives LOW output
+    let dt_pin = pins.gpio8.into_floating_input();
+    let clk_pin = pins.gpio9.into_floating_input();
     let mut led_pin = pins.led.into_push_pull_output();
     let mut npl_led_bit_a = pins.gpio3.into_push_pull_output();
     let mut npl_led_bit_b = pins.gpio4.into_push_pull_output();
-    let mut rotation_start = clk_pin.is_low().unwrap();
+    let mut rotation_start = clk_pin.is_high().unwrap();
     let mut rotation_now;
     // test "write to I/O-port" and pushing a button --> OK
     loop {
+        // SW logic is negative
         if sw_pin.is_low().unwrap() {
             led_pin.set_high().unwrap();
         } else {
             led_pin.set_low().unwrap();
         }
-        rotation_now = clk_pin.is_low().unwrap();
+        rotation_now = clk_pin.is_high().unwrap();
         if rotation_now != rotation_start {
             // Use the DT pin to found out the direction.
             // The equivalent code in MicroPython needs debouncing, but at
             // least with two indicator LEDs this code seems fine.
-            if dt_pin.is_low().unwrap() != rotation_now {
+            if dt_pin.is_high().unwrap() != rotation_now {
                 // clockwise
                 npl_led_bit_a.set_low().unwrap();
                 npl_led_bit_b.set_high().unwrap();

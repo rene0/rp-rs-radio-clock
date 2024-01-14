rene0/rp-rs-radio-clock

commit 613e3467fae4fec5556d34a5c9e14c445bd104ce

Update date/time for MSF at 95% of the new second.

clock/src/main.rs
@@ -394,7 +394,7 @@ fn main() -> ! {
                     &mut delay,
                 );
             }
-            if msf_tick == FRAMES_PER_SECOND * 9 / 10 && msf.get_new_minute() {
+            if msf_tick == FRAMES_PER_SECOND * 19 / 20 && msf.get_new_minute() {
                 // print date/time/status
                 msf.decode_time(true);
                 if !msf.get_first_minute() {

commit c3725308b12497dd8bc917d693768a342c7e0b47

Make calculation fit in an u8
Maybe just use "raw" values for msf_tick and dcf77_tick instead of relative to FRAMES_PER_SECOND ?

Fixes: 613e3467

clock/src/main.rs
@@ -394,7 +394,7 @@ fn main() -> ! {
                     &mut delay,
                 );
             }
-            if msf_tick == FRAMES_PER_SECOND * 19 / 20 && msf.get_new_minute() {
+            if msf_tick == ((FRAMES_PER_SECOND as u16 * 19) / 20) as u8 && msf.get_new_minute() {
                 // print date/time/status
                 msf.decode_time(true);
                 if !msf.get_first_minute() {